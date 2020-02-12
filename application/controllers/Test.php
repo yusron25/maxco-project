117992befd07c420741323fab847cd974ef04b31
Muhammad Yusron <muhammadyusron17@gmail.com>
2020-02-12 09:32:58 +0700

    Update Test.php

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -6,4 +6,21 @@ class Test extends CI_Controller {
 	public function index(){
 		echo $_SESSION['language'];
 	}
+
+	public function ubahsession(){
+
+		$d = date('H-i-s')
+		session_start();();
+		$_SESSION['nativ'] = $d;
+
+		$this->session->set_userdata('ci__' ,$d);
+		
+	}
+
+	public function vsess(){
+		session_start();
+		echo json_encode($_SESSION);
+	}
+
+
 }
